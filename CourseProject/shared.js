59def21acd7acb6622ced3f4820bb4db58de089e
dmitru4ok <mobilokh4566@gmail.com>
2023-12-02 12:28:16 +0200

    JS animation event listener understood

--- CourseProject/shared.js
+++ CourseProject/shared.js
@@ -4,6 +4,7 @@ let allChoosenButtons = document.querySelectorAll(".plan .button");
 let discardPlanChoosingBtn = document.querySelector(".modal__action--negative");
 let togleBtn = document.querySelector(".toggle-button");
 let mobileNav = document.querySelector(".mobile-nav");
+let ctaButton = document.querySelector(".main-nav__item:last-child"); 
 
 const closeModal = () => {
     backdrop.classList.remove('display');
@@ -47,8 +48,19 @@ togleBtn.addEventListener('click', () => {
         backdrop.classList.add('display');
         mobileNav.classList.add('display');
     }, 1);
-    
 });
+
+
+ctaButton.addEventListener('animationstart', (eventData) => {
+    //animationend as well
+    console.log('started', eventData);
+});
+
+
+
+// ctaButton.addEventListener('animationiteration', (eventData) => {
+//     console.log('iteration', eventData);
+// });
 
 
 
